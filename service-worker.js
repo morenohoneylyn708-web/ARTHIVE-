0ea151f8c8bbbff30b6c3d83a2f7231eb0c12a0c
morenohoneylyn708-web <morenohoneylyn708@gmail.com>
2025-12-14 19:02:08 +0800

    Arthive
    Discovering the world of showcasing Arthive

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'arthive-cache-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
